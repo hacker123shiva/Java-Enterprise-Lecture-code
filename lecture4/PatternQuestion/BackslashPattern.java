7f73c7557e44d3bccbabbb49d4e6c02d44bbdf5d
Shiva srivastava <105240067+hacker123shiva@users.noreply.github.com>
2022-07-19 18:23:51 +0530

    Add files via upload

---
+++ lecture4/PatternQuestion/BackslashPattern.java
@@ -0,0 +1,17 @@
+
+public class BackslashPattern {
+
+	public static void main(String[] args) {
+	int n=5;
+	for(int i=1;i<=5;i++)
+	{for(int j=1;j<=5;j++)
+		if(i==j)
+			System.out.print("* ");
+		else 
+			System.out.print("  ");
+	System.out.println();
+	}
+
+	}
+
+}
