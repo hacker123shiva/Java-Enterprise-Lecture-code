9dd1232e5a4999e910037d4c92e2c5526d181406
Shiva srivastava <105240067+hacker123shiva@users.noreply.github.com>
2022-08-01 11:42:46 +0530

    add range

--- project/guesserGameProject/Player.java
+++ project/guesserGameProject/Player.java
@@ -5,7 +5,7 @@ private int guess_number;
 public int guessNumber()
 {
 	Scanner sc =new Scanner(System.in);
-	System.out.println("Player guess the number");
+	System.out.println("Player guess the number range between 0 to 10");
 	guess_number=sc.nextInt();
 	return guess_number;
 }
